d9a60357ffa608801a8dc86d76e538d22452089e
Frederico Marino <fmarino1995@outlook.com>
2022-03-24 20:35:26 -0300

    Adição de Jquery , Data Table

---
+++ SistemaAcademicoCore/Pages/Users/Users.razor.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using SistemaAcademicoApplication.Usuarios.Queries;
+
+namespace SistemaAcademicoWeb.Pages.Users
+{
+    public class UsersBase : ComponentBase
+    {
+        [Inject]
+        public IMediator _mediator { get; set; }
+
+
+        protected List<IdentityUser> Usuarios = new List<IdentityUser>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var usuariosList = await _mediator.Send(new ObterUsuariosQuery());
+
+            Usuarios = usuariosList.Result;
+        }
+    }
+}
---
+++ SistemaAcademicoCore/wwwroot/js/JSInterop.js
@@ -0,0 +1,24 @@
+﻿function AddDataTable(table, searching) {
+    $(table).DataTable({
+        "retrieve": true,
+        "searching": searching,
+        "ordering": false,
+        "pagingType": "full_numbers",
+        "iDisplayLength": 50,
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "emptyTable": "Nenhum registro disponível na tabela",
+            "infoFiltered": "(Filtrando de _MAX_ total de registros)",
+            "zeroRecords": "Nenhum registro encontrado",
+            "search": "Pesquisar",
+            "infoEmpty": "Mostrando 0 até 0 de 0 registros",
+            "info": "Mostrando _START_ até _END_ de _TOTAL_ registros",
+            "paginate": {
+                "first": "Primeira página",
+                "last": "Última página ",
+                "next": "Próximo",
+                "previous": "Anterior"
+            }
+        }
+    });
+}
