ec327d27a85b7640e7775fa3233acd8a66d1e2c0
Frederico Marino <fmarino1995@outlook.com>
2022-03-30 20:46:04 -0300

    desenvolvendo o footer da aplicação

--- SistemaAcademicoCore/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SistemaAcademicoCore/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -64,8 +64,7 @@ namespace SistemaAcademicoCore.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required(ErrorMessage = "Digite seu email")]
-            [EmailAddress(ErrorMessage = "Formato incorreto")]
+            [EmailAddress]
             public string Email { get; set; }
 
             /// <summary>
