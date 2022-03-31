8140de844c1d7c5c72c91047906587e103972c8e
Frederico Duarte <frederico.duarte@brwo.com.br>
2022-03-31 11:58:26 -0300

    Alterações nos modelos

--- Domain/Entities/Professor.cs
+++ Domain/Entities/Professor.cs
@@ -1,7 +1,10 @@
-﻿namespace Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
 {
     public class Professor
     {
+        [Key]
         public int ProfessorId { get; set; }
         public int FuncionarioId { get; set; }
         public virtual Funcionario Funcionario { get; set; }
