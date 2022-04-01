97fca88f8c30fdf7920afc6ef6775206429fa182
Frederico Marino <fmarino1995@outlook.com>
2022-03-31 22:35:30 -0300

    Acertando as paginas do identity

--- SistemaAcademicoCore/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ SistemaAcademicoCore/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -20,12 +20,10 @@ namespace SistemaAcademicoCore.Areas.Identity.Pages.Account
     public class ForgotPasswordModel : PageModel
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly IEmailSender _emailSender;
 
-        public ForgotPasswordModel(UserManager<ApplicationUser> userManager, IEmailSender emailSender)
+        public ForgotPasswordModel(UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
-            _emailSender = emailSender;
         }
 
         /// <summary>
@@ -71,10 +69,10 @@ namespace SistemaAcademicoCore.Areas.Identity.Pages.Account
                     values: new { area = "Identity", code },
                     protocol: Request.Scheme);
 
-                await _emailSender.SendEmailAsync(
-                    Input.Email,
-                    "Reset Password",
-                    $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                //await _emailSender.SendEmailAsync(
+                //    Input.Email,
+                //    "Reset Password",
+                //    $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                 return RedirectToPage("./ForgotPasswordConfirmation");
             }
