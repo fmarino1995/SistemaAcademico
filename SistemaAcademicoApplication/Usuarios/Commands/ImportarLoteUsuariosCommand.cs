f8b4273c46656c5dc780f8119979b70f3d16d524
Frederico Marino <fmarino1995@outlook.com>
2022-04-07 19:19:17 -0300

    Adicao de modelo de importacao de usuarios

--- SistemaAcademicoApplication/Usuarios/Commands/ImportarLoteUsuariosCommand.cs
+++ SistemaAcademicoApplication/Usuarios/Commands/ImportarLoteUsuariosCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using SistemaAcademicoApplication.Common.Responses;
 using System;
 using System.Collections.Generic;
@@ -10,11 +11,13 @@ namespace SistemaAcademicoApplication.Usuarios.Commands
 {
     public class ImportarLoteUsuariosCommand : IRequest<Response<bool>>
     {
-
+        public IFormFile File { get; set; }
     }
 
     public class ImportarLoteUsuariosCommandHanndler
     {
+
+
 
     }
 }
