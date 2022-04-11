ec8b8259c9c69c19b2b655592acb8b72c4d3fa2b
Frederico Marino <fmarino1995@outlook.com>
2022-04-11 20:44:45 -0300

    aalterações nas paginas do Identity

--- SistemaAcademicoCore/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ SistemaAcademicoCore/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -57,7 +57,7 @@ namespace SistemaAcademicoCore.Areas.Identity.Pages.Account.Manage
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Phone]
-            [Display(Name = "Phone number")]
+            [Display(Name = "Número de celular")]
             public string PhoneNumber { get; set; }
         }
 
