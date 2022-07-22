8b9447ccad534bf9ed0c51a6e898db406acec240
Frederico Duarte <frederico.duarte@brwo.com.br>
2022-07-22 12:10:35 -0300

    CRUD PROFESSOR em desenvolvimento

---
+++ SistemaAcademicoApplication/Professores/Queries/ObterProfessorQuery.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using SistemaAcademicoData.Context;
+using SistemaAcademicoApplication.Common.Responses;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace SistemaAcademicoApplication.Professores.Queries
+{
+    public class ObterProfessorQuery : IRequest<Response<Professor>>
+    {
+        public int ProfessorId { get; set; }
+    }
+
+    public class ObterProfessorQueryHandler : IRequestHandler<ObterProfessorQuery, Response<Professor>>
+    {
+        private readonly SistemaAcademicoContext _context;
+
+        public ObterProfessorQueryHandler(SistemaAcademicoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response<Professor>> Handle(ObterProfessorQuery request, CancellationToken cancellationToken)
+        {
+            var professor = await _context.Professores
+                .Include(a => a.Endereco)
+                .Include(a => a.ApplicationUser)
+                .FirstOrDefaultAsync(a => a.ProfessorId == request.ProfessorId);
+
+            if (professor == null)
+            {
+                var errorResponse = new Response<Professor>();
+                errorResponse.AddError("Professor não encontrado");
+                return errorResponse;
+            }
+
+            return new Response<Professor>(professor);
+        }
+    }
+}
