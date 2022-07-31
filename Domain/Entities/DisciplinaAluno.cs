c6cd336a78e51359e66d5615279b68b5ab51ee03
Frederico Marino <fmarino1995@outlook.com>
2022-07-31 12:08:16 -0300

    Alterações na tabela disciplina alunos + migration

--- Domain/Entities/DisciplinaAluno.cs
+++ Domain/Entities/DisciplinaAluno.cs
@@ -11,5 +11,7 @@ namespace Domain.Entities
         public int DisciplinaId { get; set; }
         public virtual Disciplina Disciplina { get; set; }
         public int TotalAulasValidas { get; set; }
+        public int Ano { get; set; }
+        public int Semestre { get; set; }
     }
 }
