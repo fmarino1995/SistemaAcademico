c2dfc1f0999c79bd272ca5f5f2aa28da3e99e907
Frederico Duarte <frederico.duarte@brwo.com.br>
2022-08-10 11:30:29 -0300

    alterações na pauta web professor

--- SistemaAcademicoApplication/DisciplinaAlunos/Queries/ObterDisciplinasAlunosProfessorQuery.cs
+++ SistemaAcademicoApplication/DisciplinaAlunos/Queries/ObterDisciplinasAlunosProfessorQuery.cs
@@ -12,13 +12,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace SistemaAcademicoApplication.DisciplinaAlunos.Queries
 {
-    public class ObterDisciplinasAlunosProfessorQuery : IRequest<Response<DisciplinaAlunoProfessorViewModel>>
+    public class ObterDisciplinasAlunosProfessorQuery : IRequest<Response<List<DisciplinaAluno>>>
     {
         public int ProfessorId { get; set; }
         public int DisciplinaId { get; set; }
+        public string Turno { get; set; }
     }
 
-    public class ObterDisciplinasAlunosProfessorQueryHandler : IRequestHandler<ObterDisciplinasAlunosProfessorQuery, Response<DisciplinaAlunoProfessorViewModel>>
+    public class ObterDisciplinasAlunosProfessorQueryHandler : IRequestHandler<ObterDisciplinasAlunosProfessorQuery, Response<List<DisciplinaAluno>>>
     {
         private readonly SistemaAcademicoContext _context;
 
@@ -27,18 +28,25 @@ namespace SistemaAcademicoApplication.DisciplinaAlunos.Queries
             _context = context;
         }
 
-        public async Task<Response<DisciplinaAlunoProfessorViewModel>> Handle(ObterDisciplinasAlunosProfessorQuery request, CancellationToken cancellationToken)
+        public async Task<Response<List<DisciplinaAluno>>> Handle(ObterDisciplinasAlunosProfessorQuery request, CancellationToken cancellationToken)
         {
-            
-            
-                               
-
-                               
-                               
+            var semestre = DateTime.Now.Month <= 6 ? 1 : 2; // criar tabela para semestre vigente .
 
 
-            return new Response<DisciplinaAlunoProfessorViewModel>();
-                               
+            var disciplinasAlunos = await _context.DisciplinasAlunos
+                .Include(d => d.Disciplina)
+                .Include(d => d.Aluno)
+                .Where(d => d.DisciplinaId == request.DisciplinaId && d.Ano == DateTime.Now.Year && d.Disciplina.Turno == request.Turno)
+                .ToListAsync();
+
+            if (disciplinasAlunos == null)
+            {
+                var errorResponse = new Response<List<DisciplinaAluno>>();
+                errorResponse.AddError("Nnehum registro encontrado");
+                return errorResponse;
+            }
+
+            return new Response<List<DisciplinaAluno>>(disciplinasAlunos);
         }
     }
 }
