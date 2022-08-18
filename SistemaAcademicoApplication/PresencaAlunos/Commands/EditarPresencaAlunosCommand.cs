48a5012af4a19280b7aca35bf8db4f1afc26bbb1
Frederico Duarte <frederico.duarte@brwo.com.br>
2022-08-18 09:39:06 -0300

    PautaWeb Professor pronto para testes

--- SistemaAcademicoApplication/PresencaAlunos/Commands/EditarPresencaAlunosCommand.cs
+++ SistemaAcademicoApplication/PresencaAlunos/Commands/EditarPresencaAlunosCommand.cs
@@ -30,14 +30,35 @@ namespace SistemaAcademicoApplication.PresencaAlunos.Commands
         {
             try
             {
-                foreach(var item in request.ViewModel.DisciplinasAlunoList)
+                foreach (var item in request.ViewModel.DisciplinasAlunoList)
                 {
-                    var faltaOld = request.ViewModel.PresencaAlunos
-                        .Where(x => x.AlunoId == item.AlunoId).FirstOrDefault();
-
                     var presencaOld = request.ViewModel.PresencaAlunos
                         .Where(x => x.AlunoId == item.AlunoId).FirstOrDefault();
+
+                    if (item.IsFalta)
+                    {
+                        item.QuantidadePresenca--;
+                        item.QuantidadeFalta++;
+                        item.DataUltimaPresenca = new DateTime(0001, 01, 01);
+                        presencaOld.Falta = item.IsFalta;
+                        presencaOld.Presenca = false;
+                    }
+
+                    if (item.IsPresenca)
+                    {
+                        item.QuantidadeFalta--;
+                        item.QuantidadePresenca++;
+                        item.DataUltimaPresenca = DateTime.Now;
+                        presencaOld.Presenca = item.IsPresenca;
+                        presencaOld.Falta = false;
+                    }
+
+                    _context.DisciplinasAlunos.Update(item);
+                    _context.PresencaAlunos.Update(presencaOld);
                 }
+
+                await _context.SaveChangesAsync();
+                return new Response<bool>(true);
             }
             catch (Exception ex)
             {
@@ -45,11 +66,6 @@ namespace SistemaAcademicoApplication.PresencaAlunos.Commands
                 errorResponse.AddError(ex.Message);
                 return errorResponse;
             }
-        }
-
-        private PresencaAluno FindPresencaAluno(int id)
-        {
-            return _context.PresencaAlunos
         }
     }
 }
