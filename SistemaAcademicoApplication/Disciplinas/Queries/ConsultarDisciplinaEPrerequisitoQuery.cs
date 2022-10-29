3eb6021c90f9f44dc55c3c4190d69ea3ac44ea8e
Frederico Marino <fmarino1995@outlook.com>
2022-10-29 15:49:26 -0300

    mais ajustes finais

---
+++ SistemaAcademicoApplication/Disciplinas/Queries/ConsultarDisciplinaEPrerequisitoQuery.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SistemaAcademicoApplication.Common.Responses;
+using SistemaAcademicoData.Context;
+
+namespace SistemaAcademicoApplication.Disciplinas.Queries
+{
+    public class ConsultarDisciplinaEPrerequisitoQuery : IRequest<Response<Disciplina>>
+    {
+        public string Matricula { get; set; }
+        public string Turno { get; set; }
+    }
+
+    public class ConsultarDisciplinaEPrerequisitoQueryHandler : IRequestHandler<ConsultarDisciplinaEPrerequisitoQuery, Response<Disciplina>>
+    {
+        private readonly SistemaAcademicoContext _context;
+
+        public ConsultarDisciplinaEPrerequisitoQueryHandler(SistemaAcademicoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response<Disciplina>> Handle(ConsultarDisciplinaEPrerequisitoQuery request, CancellationToken cancellationToken)
+        {
+            var disciplina = await _context.Disciplinas
+                .Where(d => d.PreRequisito == request.Matricula && d.Turno == request.Turno)
+                .FirstOrDefaultAsync();
+
+            if (disciplina == null)
+            {
+                var errorResponse = new Response<Disciplina>();
+                errorResponse.AddError("Nenhuma disciplina pré-requisito encontrada");
+                return errorResponse;
+            }
+
+            return new Response<Disciplina>(disciplina);
+        }
+    }
+}
