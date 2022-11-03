310c23a7049276d72f9688325116c26ec6cd29aa
Frederico Marino <fmarino1995@outlook.com>
2022-11-03 20:42:25 -0300

    ajustes finais

---
+++ Domain/ValidationEntities/AddAvisoValidator.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Domain.ValidationEntities
+{
+    public class AddAvisoValidator : AbstractValidator<Aviso>
+    {
+        public AddAvisoValidator()
+        {
+            RuleFor(x => x.Titulo)
+                .NotEmpty().NotNull().WithMessage("Digite o título do aviso");
+
+            RuleFor(x => x.Texto)
+                .NotEmpty().NotNull().WithMessage("Digite o título do aviso");
+
+            RuleFor(x => x.EmailDestino)
+                .NotEmpty().NotNull().WithMessage("Digite o título do aviso");
+        }
+    }
+}
